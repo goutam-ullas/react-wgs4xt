a429d26ac97f220011b9dc2874c367513cdd015c
goutam-ullas <goutam.ullas@gmail.com>
2020-10-11 00:14:02 +0200

    Oct 11 | 00:13

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class Application extends React.Component {
       popUpColor: "",
       scaleDistance: 13,
       themeDescWidth: "40%",
-      themeDescTop : 500
+      themeDescTop : 500,
       themeVidLeft: 0.05 * window.innerWidth,
       themeDescLeft: 0.5 * window.innerWidth,
       themeVidRight: 0.29 * window.innerWidth,
