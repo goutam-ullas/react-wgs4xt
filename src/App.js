bef36a3fb6203d4c7d820be4963a6cf0b82065d4
goutam-ullas <goutam.ullas@gmail.com>
2020-10-12 16:11:47 +0200

    Oct 12 | 16:11

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class Application extends React.Component {
       popUpPad: 0,
       popUpColor: "",
       scaleDistance: 13,
-      themeDescWidth: "40%",
+      themeDescWidth: "30%",
       themeVidLeft: 18,
       themeDescLeft: 0.5 * window.innerWidth,
       page1Vis: "visible",
@@ -846,8 +846,8 @@ class Application extends React.Component {
           style={{
             position: "fixed",
             pointerEvents: "none",
-            top: 0,
             left: 0,
+            top:0,
             height: window.innerHeight,
             width: window.innerWidth,
             visibility: this.state.page2Vis
@@ -856,11 +856,11 @@ class Application extends React.Component {
           {/*Theme 1 Description*/}
           <div
             style={{
-              padding: 20,
+              display: "inline-block",
               position: "absolute",
+              top: this.state.mapHeight/5,
               zIndex: 5,
               width: this.state.themeDescWidth,
-              top: this.state.mapHeight/3,
               left: this.state.themeDescLeft,
               transition: "width 1s, bottom 1s, left 1s"
             }}
@@ -872,71 +872,15 @@ class Application extends React.Component {
             className="video"
             ref={el => (this.video1Ref = el)}
             style={{
-              top: this.state.mapHeight / 4,
-              left: 0,
+              display: "inline-block",
+              top: this.state.mapHeight/5,
+              left: 35,
               zIndex: this.state.videoZindex1
             }}
             url={this.theme1Video}
             controls={true}
           />
         </div>
-
-        {/*Page 3: Theme 2
-        <div
-          style={{
-            position: "fixed",
-            pointerEvents: "none",
-            top: 0,
-            left: 0,
-            height: window.innerHeight,
-            width: window.innerWidth,
-            visibility: this.state.page3Vis,
-            backgroundColor: "transparent"
-          }}
-        >*/}
-        {/*Theme 2 - Background image
-          <img
-            style={{
-              position: "fixed",
-              backgroundColor: "transparent",
-              bottom: 0,
-              left: -10,
-              width: 1.02 * this.state.mapWidth,
-              zIndex: 1
-            }}
-            src={this.theme2Img}
-          />*/}
-        {/*Theme 2 Description
-          <div
-            style={{
-              padding: 20,
-              position: "fixed",
-              zIndex: 5,
-              width: "40%",
-              bottom: this.state.mapHeight / 10,
-              left: this.state.themeVidRight,
-              transition: "width 1s, bottom 1s, left 1s"
-            }}
-          >
-            <text className="themeDesc">{this.theme2Desc}</text>
-          </div>*/}
-        {/*Theme 2 Video
-          <ReactPlayer
-            className="video"
-            style={{
-              bottom: this.state.mapHeight / 4,
-              zIndex: this.state.videoZindex3,
-              left: this.state.themeVidRight - 20,
-              transition: "left 1s"
-            }}
-            height={this.state.videoHeight}
-            width={this.state.videoWidth}
-            url={this.theme2Video}
-            controls={true}
-            onPlay={() => this.setState({ videoZindex3: 10 })}
-            onPause={() => this.setState({ videoZindex3: 0 })}
-          />
-        </div>*/}
 
         {/*Page 4: Theme 3*/}
         <div
@@ -953,12 +897,12 @@ class Application extends React.Component {
           {/*Theme 3 Description*/}
           <div
             style={{
-              padding: 20,
               position: "absolute",
+              display: "inline-block",
+              top: this.state.mapHeight/5,
               zIndex: 5,
               width: this.state.themeDescWidth,
-              top: this.state.mapHeight/3,
-              left: this.state.themeDescLeft,
+              left: this.state.themeDescLeft + 50,
               transition: "width 1s, top 1s, left 1s"
             }}
           >
@@ -980,8 +924,9 @@ class Application extends React.Component {
             className="video"
             ref={el => (this.video1Ref = el)}
             style={{
-              top: this.state.mapHeight / 4,
-              left: this.state.themeVidLeft,
+              display: "inline-block",
+              top: this.state.mapHeight/5,
+              left: 65,
               zIndex: this.state.videoZindex1,
               transition: "top 1s"
             }}
@@ -1005,12 +950,12 @@ class Application extends React.Component {
           {/*Theme 4 Description*/}
           <div
             style={{
-              padding: 20,
               position: "absolute",
+              display: "inline-block",
+              top: this.state.mapHeight/5,
               zIndex: 5,
               width: this.state.themeDescWidth,
-              top: this.state.mapHeight/3,
-              left: this.state.themeDescLeft,
+              left: this.state.themeDescLeft - 150,
               transition: "width 1s, bottom 1s"
             }}
           >
@@ -1020,8 +965,9 @@ class Application extends React.Component {
           <ReactPlayer
             className="video"
             style={{
-              top: this.state.mapHeight / 4,
-              left: this.state.themeVidLeft,
+              display: "inline-block",
+              top: this.state.mapHeight/5,
+              left: -35,
               zIndex: this.state.videoZindex4
             }}
             url={this.theme4Video}
@@ -1043,12 +989,12 @@ class Application extends React.Component {
           {/*Theme 4 Description*/}
           <div
             style={{
-              padding: 20,
               position: "absolute",
+              display: "inline-block",
+              top: this.state.mapHeight/5,
               zIndex: 5,
               width: this.state.themeDescWidth,
-              top: this.state.mapHeight/3,
-              left: this.state.themeDescLeft,
+              left: this.state.themeDescLeft + 35,
               transition: "width 1s, bottom 1s"
             }}
           >
@@ -1058,8 +1004,9 @@ class Application extends React.Component {
           <ReactPlayer
             className="video"
             style={{
-              top: this.state.mapHeight / 4,
-              left: this.state.themeVidLeft,
+              display: "inline-block",
+              top: this.state.mapHeight/5,
+              left: 70,
               zIndex: this.state.videoZindex4
             }}
             url={this.theme5Video}
@@ -1081,11 +1028,11 @@ class Application extends React.Component {
           {/*Theme 4 Description*/}
           <div
             style={{
-              padding: 20,
               position: "absolute",
+              display: "inline-block",
+              top: this.state.mapHeight/5,
               zIndex: 5,
               width: this.state.themeDescWidth,
-              top: this.state.mapHeight/3,
               left: this.state.themeDescLeft,
               transition: "width 1s, bottom 1s"
             }}
@@ -1096,8 +1043,9 @@ class Application extends React.Component {
           <ReactPlayer
             className="video"
             style={{
-              top: this.state.mapHeight / 4,
-              left: this.state.themeVidLeft,
+              display: "inline-block",
+              top: this.state.mapHeight/5,
+              left: 35,
               zIndex: this.state.videoZindex4
             }}
             url={this.theme6Video}
@@ -1119,12 +1067,12 @@ class Application extends React.Component {
           {/*Theme 4 Description*/}
           <div
             style={{
-              padding: 20,
               position: "absolute",
+              display: "inline-block",
+              top: this.state.mapHeight/5,
               zIndex: 5,
               width: this.state.themeDescWidth,
-              top: this.state.mapHeight/3,
-              left: this.state.themeDescLeft,
+              left: this.state.themeDescLeft ,
               transition: "width 1s, bottom 1s"
             }}
           >
@@ -1134,8 +1082,9 @@ class Application extends React.Component {
           <ReactPlayer
             className="video"
             style={{
-              top: this.state.mapHeight / 4,
-              left: this.state.themeVidLeft,
+              display: "inline-block",
+              top: this.state.mapHeight/5,
+              left: 70,
               zIndex: this.state.videoZindex4
             }}
 
@@ -1591,7 +1540,7 @@ class Application extends React.Component {
             aria-label=""
             onClick={this.researchFunction}
             style={{
-              fontSize: 1,
+              fontSize: 16,
               position: "fixed",
               marginTop: 10,
               marginLeft: 10,
